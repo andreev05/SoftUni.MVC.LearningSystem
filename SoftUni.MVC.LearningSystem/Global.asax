ad5b5c6caff2d20974c11ea38a4246dffbaf57c3
fbobg05 <fbo_bg1@mailinator.com>
2017-08-10 15:46:10 +0300

    Add project files.

---
+++ SoftUni.MVC.LearningSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftUni.MVC.LearningSystem.Web.MvcApplication" Language="C#" %>
---
+++ SoftUni.MVC.LearningSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoftUni.MVC.LearningSystem.Web.Startup))]
+namespace SoftUni.MVC.LearningSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
