0b61d07bf86786a38216e6442183d2fce3272093
fbobg05 <fbo_bg1@mailinator.com>
2017-08-14 13:44:07 +0300

    added roles with seed

---
+++ SoftUni.MVC.LearningSystem.Data/Migrations/Configuration.cs
@@ -0,0 +1,58 @@
+namespace SoftUni.MVC.LearningSystem.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using SoftUni.MVC.LearningSystem.Data;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LearningSystemContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(LearningSystemContext context)
+        {
+            if(!context.Roles.Any(r => r.Name == "Student"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole("Student");
+                manager.Create(role);
+            }
+
+            if(!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole("Admin");
+                manager.Create(role);
+            }
+
+            if(!context.Roles.Any(r => r.Name == "Trainer"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole("Trainer");
+                manager.Create(role);
+            }
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
